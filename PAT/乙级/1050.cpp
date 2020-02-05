359aa2c91ab9ed5eefa04a867a264616b400b1c5
muyuuuu <muyu0223@outlook.com>
2020-02-05 19:37:07 +0800

    finish basic 1050

--- PAT/乙级/1050.cpp
+++ PAT/乙级/1050.cpp
@@ -1,40 +1,133 @@
 #include <iostream>
 #include <algorithm>
-#include <cmath>
 #include <vector>
+#include <cmath>
+
 using namespace std;
-int cmp(int a, int b) {return a > b;}
-int main() {
-    int N, m, n, t = 0;
-    scanf("%d", &N);
-    for (n = sqrt((double)N); n >= 1; n--) {
-        if (N % n == 0) {
-            m = N / n;
+
+int arr[101][101];
+
+bool cmp(int a, int b)
+{
+    if (a > b)
+        return true;
+    else
+        return false;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int temp;
+    int min = 10000;
+    int row = 0, col = 0;
+    vector<int> v;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> temp;
+        v.push_back(temp);
+    }
+    sort(v.begin(), v.end(), cmp);
+    int m = sqrt(n);
+    for (int i = m; i > 0; i--)
+    {
+        for (int j = m; j <= n; j++)
+        {
+            if (i * j == n)
+            {
+                if (j - i <= min)
+                {
+                    min = j - i;
+                    row = j;
+                    col = i;
+                }
+            }
+        }
+    }
+    int key = 0;
+    int flag = 0, col_flag = 0;
+    int a = row, b = col;
+    if (col == 1)
+    {
+        for (auto it = v.begin(); it != v.end(); it++)
+        {
+            cout << *it << endl;
+        }
+        return 0;
+    }
+    if (row == 2)
+    {
+        cout << v[0] << " " << v[1] << endl;
+        cout << v[3] << " " << v[2] << endl;
+        return 0;
+    }
+    while(col > 1 && flag < row)
+    {
+        int i = flag;
+        for (int j = flag; j < col; j++)
+        {
+            arr[i][j] = v[key];
+            key++;
+            // 左到右
+            // cout << i << "  "<< j << "  " << arr[i][j] << endl;
+            if (j == col - 1)
+            {
+                i += 1;
+                break;
+            }
+        }
+        for (int k = i; k < row; k++)
+        {
+            arr[k][col - 1] = v[key];
+            // 右到下
+            // cout << k << "  "<< col - 1 << "  " << arr[k][col - 1] << endl;
+            key ++;
+            if (k == row - 1)
+            {
+                if (flag == row - 1)
+                    break;
+                for (int j = col - 2; j > flag; j--)
+                {
+                    arr[k][j] = v[key];
+                    // 下到左
+                    // cout << k << "  "<< j << "  " << arr[k][j] << endl;
+                    key ++ ;
+                }
+            }
+        }
+        // 左到上
+        for (int l = row - 1; l > flag; l--)
+        {
+            if (flag == col - 1)
+                break;
+            else
+            {
+                arr[l][flag] = v[key];
+                // cout << l << "  " << flag << "  " << v[key] << endl;
+                key++;
+            }
+        }
+        row = row - 1;
+        if (col == 2 && col_flag == 0)
+        {
+            arr[1][flag] = v[key];
             break;
         }
+        col_flag = 1;
+        col = col - 1;
+        flag ++;
     }
-    vector<int> a(N);
-    for (int i = 0; i < N; i++)
-        scanf("%d", &a[i]);
-    sort(a.begin(), a.end(), cmp);
-    vector<vector<int> > b(m, vector<int>(n));
-    int level = m / 2 + m % 2;
-    for (int i = 0; i < level; i++) {
-        for (int j = i; j <= n - 1 - i && t <= N - 1; j++)
-                b[i][j] = a[t++];
-        for (int j = i + 1; j <= m - 2 - i && t <= N - 1; j++)
-                b[j][n - 1 - i] = a[t++];
-        for (int j = n - i - 1; j >= i && t <= N - 1; j--)
-                b[m - 1 - i][j] = a[t++];
-        for (int j = m - 2 - i; j >= i + 1 && t <= N - 1; j--)
-                b[j][i] = a[t++];
-    }
-    for (int i = 0; i < m; i++) {
-        for (int j = 0 ; j < n; j++) {
-            printf("%d", b[i][j]);
-            if (j != n - 1) printf(" ");
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            cout << arr[i][j];
+            if (j != b - 1)
+                cout << " ";
         }
-        printf("\n");
+        if (i != a - 1)
+            cout << endl;
     }
     return 0;
 }
