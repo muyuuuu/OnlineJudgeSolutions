ab51b3b47ba52adbf33df83299c813f372f6c0a2
muyuuuu <muyu0223@outlook.com>
2020-04-17 14:47:40 +0800

    finish advanced 1064

---
+++ PAT/甲级/1064.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int in[1010], level[1010], n, t = 0;
+
+void inOrder(int root) 
+{
+    if (root >= n) 
+        return ;
+    inOrder(root * 2 + 1);
+    level[root] = in[t++];
+    inOrder(root * 2 + 2);
+}
+
+int main() {
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &in[i]);
+    sort(in, in + n);
+    inOrder(0);
+    printf("%d", level[0]);
+    for (int i = 1; i < n; i++)
+        printf(" %d", level[i]);
+    return 0;
+}
--- PAT/甲级/test.cpp
+++ PAT/甲级/test.cpp
@@ -1,45 +1,64 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
-#include <map>
+
 using namespace std;
-struct node {
-    string name;
-    int gp, gm, gf, g;
+
+struct node
+{
+    int v;
+    struct node *left, *right; 
 };
-bool cmp(node a, node b) {
-    return a.g != b.g ? a.g > b.g : a.name < b.name;
+
+int arr[1002], arr1[1002], arr2[1002];
+int key1 = 0, key2 = 0;
+
+// 建立查找树
+node* build(node *root, int v)
+{
+    if (root == NULL)
+    {
+        root = new node();
+        root->v = v;
+        root->left = NULL;
+        root->right = NULL;
+    }
+    else if (v < root->v)
+    {
+        root->left = build(root->left, v);
+    }
+    else
+    {
+        root->right = build(root->right, v);
+    }
+    return root;
 }
-map<string, int> idx;
-int main() {
-    int p, m, n, score, cnt = 1;
-    cin >> p >> m >> n;
-    vector<node> v, ans;
-    string s;
-    for (int i = 0; i < p; i++) {
-        cin >> s >> score;
-        if (score >= 200) {
-            v.push_back(node{s, score, -1, -1, 0});
-            idx[s] = cnt++;
-        }
+
+// 先序遍历
+void preorder(node *root, int flag)
+{
+    if (root != NULL)
+    {
+        if (flag == 0)
+            cout << root->v << " ";
+        if (flag == 1)
+            arr2[key2++] = root->v;
+        // 左 右遍历
+        preorder(root->left, flag);
+        preorder(root->right, flag);
     }
-    for (int i = 0; i < m; i++) {
-        cin >> s >> score;
-        if (idx[s] != 0) v[idx[s] - 1].gm = score;
+}
+
+
+int main(int argc, char const *argv[])
+{   
+    int n, temp, flag = 0;
+    cin >> n;
+    node *root = NULL;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> temp;
+        arr[i] = temp;
+        // 便于遍历时寻找节点
+        root = build(root, temp);
     }
-    for (int i = 0; i < n; i++) {
-        cin >> s >> score;
-        if (idx[s] != 0) {
-            int temp = idx[s] - 1;
-            v[temp].gf = v[temp].g = score;
-            if (v[temp].gm > v[temp].gf) 
-				v[temp].g = int(v[temp].gm * 0.4 + v[temp].gf * 0.6 + 0.5);
-        }
-    }
-    for (int i = 0; i < v.size(); i++)
-        if (v[i].g >= 60) ans.push_back(v[i]);
-    sort(ans.begin(), ans.end(), cmp);
-    for (int i = 0; i < ans.size(); i++)
-        printf("%s %d %d %d %d\n", ans[i].name.c_str(), ans[i].gp, ans[i].gm, ans[i].gf, ans[i].g);
-    return 0;
+    preorder(root, flag);
 }
