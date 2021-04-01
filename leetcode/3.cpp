845652c8d973308131382682c8de382b047729e0
muyuuuu <muyu0223@outlook.com>
2021-04-01 11:06:34 +0800

    leetcode 3 solution

--- leetcode/3.cpp
+++ leetcode/3.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> m(128, 0);
-        int ans = 0;
-        int i = 0;
-        for (int j = 0; j < s.size(); j++) {
-            i = max(i, m[s[j]]);
-            m[s[j]] = j + 1;
-            ans = max(ans, j - i + 1);
+        map<char, int> m;
+        int ans{0};
+        int n = s.size();
+        for (int start = 0, end = 0; end < n; end++){
+            if (m[s[end]] != 0){
+                start = max(m[s[end]], start);
+            }
+            m[s[end]] = end + 1;
+            ans = max(ans, end - start + 1);
         }
         return ans;
     }
 };
+
+// 执行用时：28 ms, 在所有 C++ 提交中击败了54.44%的用户
+// 内存消耗：8.4 MB, 在所有 C++ 提交中击败了51.68%的用户
