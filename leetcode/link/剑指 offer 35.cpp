e2f665c02427ee7cd471bda5b963490893c3a12f
muyuuuu <muyu0223@outlook.com>
2021-05-08 10:32:36 +0800

    leetcode 剑指offer35 solution :surfer:

---
+++ leetcode/link/剑指 offer 35.cpp
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        unordered_map<Node*, Node*> m;
+        Node* p = head;
+
+        while (p != nullptr) {
+            m[p] = new Node(p->val);
+            p = p->next;
+        }
+
+        p = head;
+
+        while (p != nullptr) {
+            m[p]->next = m[p->next];
+            m[p]->random = m[p->random];
+            p = p->next;
+        }
+
+        return m[head];
+    }
+};
+
+// 执行用时：8 ms, 在所有 C++ 提交中击败了89.54%的用户
+// 内存消耗：11.1 MB, 在所有 C++ 提交中击败了20.59%的用户
