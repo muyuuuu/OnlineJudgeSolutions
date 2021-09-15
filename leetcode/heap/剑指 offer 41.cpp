e1b2198464fb403aaad0a73d783705caa6b66c00
muyuuuu <muyu0223@outlook.com>
2021-09-15 10:12:34 +0800

    leetcode 295 benchmark

--- leetcode/heap/剑指 offer 41.cpp
+++ leetcode/heap/剑指 offer 41.cpp
@@ -1,7 +1,7 @@
 class MedianFinder {
 private:
-    priority_queue<int, vector<int>, less<int> > maxq;
-    priority_queue<int, vector<int>, greater<int> > minq;
+    priority_queue<int> maxq;
+    priority_queue<int, vector<int>, greater<int>> minq;
 public:
     /** initialize your data structure here. */
     MedianFinder() {
@@ -9,23 +9,22 @@ public:
     }
     
     void addNum(int num) {
-        if (minq.size() == maxq.size()) {
-            minq.push(num);
-            maxq.push(minq.top());
-            minq.pop();
-        }
-        else {
+        if (maxq.size() == minq.size()){
             maxq.push(num);
             minq.push(maxq.top());
             maxq.pop();
         }
+        else {
+            minq.push(num);
+            maxq.push(minq.top());
+            minq.pop();
+        }
     }
     
     double findMedian() {
-        if (minq.size() == maxq.size()) {
-            return (minq.top() * 1.0 + maxq.top() * 1.0) / 2;
-        }
-        return maxq.top() * 1.0;
+        if ( maxq.size() == minq.size())
+            return 1.0 * (minq.top() + maxq.top()) / 2;
+        return minq.top() * 1.0;
     }
 };
 
@@ -35,6 +34,3 @@ public:
  * obj->addNum(num);
  * double param_2 = obj->findMedian();
  */
-
-// 执行用时：132 ms, 在所有 C++ 提交中击败了46.56%的用户
-// 内存消耗：40.7 MB, 在所有 C++ 提交中击败了66.00%的用户
