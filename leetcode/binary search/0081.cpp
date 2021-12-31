8dc4c75a7dda1562fe475e81db5f0295619e8eb5
Lan-Ling <muyu0223@outlook.com>
2021-12-31 12:46:22 +0800

    update 0081 with simple solutoin

--- leetcode/binary search/0081.cpp
+++ leetcode/binary search/0081.cpp
@@ -17,25 +17,6 @@ public:
     bool search(vector<int>& a, int target) {
         set<int> s(a.begin(), a.end());
         vector<int> nums(s.begin(), s.end());
-        int l = 0, r = nums.size() - 1;
-        while (l <= r) {
-            int m = l + (r - l) / 2;
-            if (nums[m] > nums[r]) {
-                l = m + 1;
-            }
-            else if (nums[m] < nums[r]) {
-                r = m;
-            }
-            else {
-                r--;
-            }
-        }
-        if (l == 0) {
-            r = nums.size() - 1;
-            return subsearch(nums, l, r, target);
-        }
-        else if (nums[0] <= target && target <= nums[l-1])
-            return subsearch(nums, 0, l - 1, target);
-        return subsearch(nums, l, nums.size() - 1, target);
+        return subsearch(nums, 0, nums.size() - 1, target);
     }
 };
